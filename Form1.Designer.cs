b375167aaf3810b3623f424361537b887e9f985e
Drew Franklin <tdfran634@gmail.com>
2023-10-18 21:34:00 -0400

    Added ability scores, AC, and class DC fixed values and actual values.
    Several labels, comboboxes, and textboxes for character sheet info.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,50 +28,371 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.comboBox_Deity = new System.Windows.Forms.ComboBox();
+            this.comboBox_Alignment = new System.Windows.Forms.ComboBox();
+            this.textBox_Size = new System.Windows.Forms.TextBox();
+            this.textBox_XPCurrent = new System.Windows.Forms.TextBox();
+            this.textBox_PlayerName = new System.Windows.Forms.TextBox();
+            this.textBox_CharacterName = new System.Windows.Forms.TextBox();
+            this.textBox_Level = new System.Windows.Forms.TextBox();
+            this.textBox_HeroPoints = new System.Windows.Forms.TextBox();
+            this.comboBox_Background = new System.Windows.Forms.ComboBox();
+            this.textBox_ChaScore = new System.Windows.Forms.TextBox();
+            this.textBox_WisScore = new System.Windows.Forms.TextBox();
+            this.textBox_IntScore = new System.Windows.Forms.TextBox();
+            this.textBox_ConScore = new System.Windows.Forms.TextBox();
+            this.textBox_DexScore = new System.Windows.Forms.TextBox();
+            this.textBox_StrScore = new System.Windows.Forms.TextBox();
+            this.label_StrMod = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.label_DexMod = new System.Windows.Forms.Label();
+            this.label_ConMod = new System.Windows.Forms.Label();
+            this.label_IntMod = new System.Windows.Forms.Label();
+            this.label_WisMod = new System.Windows.Forms.Label();
+            this.label_ChaMod = new System.Windows.Forms.Label();
+            this.textBox_XPNeeded = new System.Windows.Forms.TextBox();
+            this.label_XPDivider = new System.Windows.Forms.Label();
+            this.label_AC = new System.Windows.Forms.Label();
+            this.label_DC = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // flowLayoutPanel1
+            // comboBox_Deity
             // 
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1232, 611);
-            this.flowLayoutPanel1.TabIndex = 0;
+            this.comboBox_Deity.FormattingEnabled = true;
+            this.comboBox_Deity.Location = new System.Drawing.Point(712, 170);
+            this.comboBox_Deity.Name = "comboBox_Deity";
+            this.comboBox_Deity.Size = new System.Drawing.Size(311, 21);
+            this.comboBox_Deity.TabIndex = 1;
+            // 
+            // comboBox_Alignment
+            // 
+            this.comboBox_Alignment.FormattingEnabled = true;
+            this.comboBox_Alignment.Location = new System.Drawing.Point(733, 145);
+            this.comboBox_Alignment.Name = "comboBox_Alignment";
+            this.comboBox_Alignment.Size = new System.Drawing.Size(88, 21);
+            this.comboBox_Alignment.TabIndex = 2;
+            // 
+            // textBox_Size
+            // 
+            this.textBox_Size.Location = new System.Drawing.Point(699, 146);
+            this.textBox_Size.Name = "textBox_Size";
+            this.textBox_Size.Size = new System.Drawing.Size(28, 20);
+            this.textBox_Size.TabIndex = 3;
+            // 
+            // textBox_XPCurrent
+            // 
+            this.textBox_XPCurrent.Location = new System.Drawing.Point(357, 169);
+            this.textBox_XPCurrent.Name = "textBox_XPCurrent";
+            this.textBox_XPCurrent.Size = new System.Drawing.Size(114, 20);
+            this.textBox_XPCurrent.TabIndex = 4;
+            // 
+            // textBox_PlayerName
+            // 
+            this.textBox_PlayerName.Location = new System.Drawing.Point(357, 127);
+            this.textBox_PlayerName.Name = "textBox_PlayerName";
+            this.textBox_PlayerName.Size = new System.Drawing.Size(256, 20);
+            this.textBox_PlayerName.TabIndex = 5;
+            // 
+            // textBox_CharacterName
+            // 
+            this.textBox_CharacterName.Location = new System.Drawing.Point(356, 55);
+            this.textBox_CharacterName.Name = "textBox_CharacterName";
+            this.textBox_CharacterName.Size = new System.Drawing.Size(257, 20);
+            this.textBox_CharacterName.TabIndex = 6;
+            // 
+            // textBox_Level
+            // 
+            this.textBox_Level.Location = new System.Drawing.Point(1047, 65);
+            this.textBox_Level.Name = "textBox_Level";
+            this.textBox_Level.Size = new System.Drawing.Size(74, 20);
+            this.textBox_Level.TabIndex = 7;
+            // 
+            // textBox_HeroPoints
+            // 
+            this.textBox_HeroPoints.Location = new System.Drawing.Point(1057, 151);
+            this.textBox_HeroPoints.Name = "textBox_HeroPoints";
+            this.textBox_HeroPoints.Size = new System.Drawing.Size(58, 20);
+            this.textBox_HeroPoints.TabIndex = 8;
+            // 
+            // comboBox_Background
+            // 
+            this.comboBox_Background.FormattingEnabled = true;
+            this.comboBox_Background.Location = new System.Drawing.Point(764, 79);
+            this.comboBox_Background.Name = "comboBox_Background";
+            this.comboBox_Background.Size = new System.Drawing.Size(259, 21);
+            this.comboBox_Background.TabIndex = 9;
+            // 
+            // textBox_ChaScore
+            // 
+            this.textBox_ChaScore.Location = new System.Drawing.Point(342, 510);
+            this.textBox_ChaScore.Name = "textBox_ChaScore";
+            this.textBox_ChaScore.Size = new System.Drawing.Size(33, 20);
+            this.textBox_ChaScore.TabIndex = 21;
+            // 
+            // textBox_WisScore
+            // 
+            this.textBox_WisScore.Location = new System.Drawing.Point(342, 455);
+            this.textBox_WisScore.Name = "textBox_WisScore";
+            this.textBox_WisScore.Size = new System.Drawing.Size(33, 20);
+            this.textBox_WisScore.TabIndex = 20;
+            // 
+            // textBox_IntScore
+            // 
+            this.textBox_IntScore.Location = new System.Drawing.Point(342, 404);
+            this.textBox_IntScore.Name = "textBox_IntScore";
+            this.textBox_IntScore.Size = new System.Drawing.Size(33, 20);
+            this.textBox_IntScore.TabIndex = 19;
+            // 
+            // textBox_ConScore
+            // 
+            this.textBox_ConScore.Location = new System.Drawing.Point(342, 350);
+            this.textBox_ConScore.Name = "textBox_ConScore";
+            this.textBox_ConScore.Size = new System.Drawing.Size(33, 20);
+            this.textBox_ConScore.TabIndex = 18;
+            // 
+            // textBox_DexScore
+            // 
+            this.textBox_DexScore.Location = new System.Drawing.Point(342, 298);
+            this.textBox_DexScore.Name = "textBox_DexScore";
+            this.textBox_DexScore.Size = new System.Drawing.Size(33, 20);
+            this.textBox_DexScore.TabIndex = 17;
+            // 
+            // textBox_StrScore
+            // 
+            this.textBox_StrScore.Location = new System.Drawing.Point(342, 239);
+            this.textBox_StrScore.Name = "textBox_StrScore";
+            this.textBox_StrScore.Size = new System.Drawing.Size(33, 20);
+            this.textBox_StrScore.TabIndex = 16;
+            // 
+            // label_StrMod
+            // 
+            this.label_StrMod.AutoSize = true;
+            this.label_StrMod.Location = new System.Drawing.Point(70, 248);
+            this.label_StrMod.Name = "label_StrMod";
+            this.label_StrMod.Size = new System.Drawing.Size(41, 13);
+            this.label_StrMod.TabIndex = 22;
+            this.label_StrMod.Text = "StrMod";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::PathFinder_2e_CharacterSheet.Properties.Resources.PZO2101_CharacterSheet_Color_1;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1208, 1583);
+            this.pictureBox1.Size = new System.Drawing.Size(1185, 1500);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label_DexMod
+            // 
+            this.label_DexMod.AutoSize = true;
+            this.label_DexMod.Location = new System.Drawing.Point(70, 301);
+            this.label_DexMod.Name = "label_DexMod";
+            this.label_DexMod.Size = new System.Drawing.Size(47, 13);
+            this.label_DexMod.TabIndex = 23;
+            this.label_DexMod.Text = "DexMod";
+            // 
+            // label_ConMod
+            // 
+            this.label_ConMod.AutoSize = true;
+            this.label_ConMod.Location = new System.Drawing.Point(70, 357);
+            this.label_ConMod.Name = "label_ConMod";
+            this.label_ConMod.Size = new System.Drawing.Size(47, 13);
+            this.label_ConMod.TabIndex = 24;
+            this.label_ConMod.Text = "ConMod";
+            // 
+            // label_IntMod
+            // 
+            this.label_IntMod.AutoSize = true;
+            this.label_IntMod.Location = new System.Drawing.Point(70, 407);
+            this.label_IntMod.Name = "label_IntMod";
+            this.label_IntMod.Size = new System.Drawing.Size(40, 13);
+            this.label_IntMod.TabIndex = 25;
+            this.label_IntMod.Text = "IntMod";
+            // 
+            // label_WisMod
+            // 
+            this.label_WisMod.AutoSize = true;
+            this.label_WisMod.Location = new System.Drawing.Point(70, 458);
+            this.label_WisMod.Name = "label_WisMod";
+            this.label_WisMod.Size = new System.Drawing.Size(46, 13);
+            this.label_WisMod.TabIndex = 26;
+            this.label_WisMod.Text = "WisMod";
+            // 
+            // label_ChaMod
+            // 
+            this.label_ChaMod.AutoSize = true;
+            this.label_ChaMod.Location = new System.Drawing.Point(70, 510);
+            this.label_ChaMod.Name = "label_ChaMod";
+            this.label_ChaMod.Size = new System.Drawing.Size(47, 13);
+            this.label_ChaMod.TabIndex = 27;
+            this.label_ChaMod.Text = "ChaMod";
+            // 
+            // textBox_XPNeeded
+            // 
+            this.textBox_XPNeeded.Location = new System.Drawing.Point(490, 169);
+            this.textBox_XPNeeded.Name = "textBox_XPNeeded";
+            this.textBox_XPNeeded.Size = new System.Drawing.Size(123, 20);
+            this.textBox_XPNeeded.TabIndex = 28;
+            // 
+            // label_XPDivider
+            // 
+            this.label_XPDivider.AutoSize = true;
+            this.label_XPDivider.Location = new System.Drawing.Point(475, 172);
+            this.label_XPDivider.Name = "label_XPDivider";
+            this.label_XPDivider.Size = new System.Drawing.Size(12, 13);
+            this.label_XPDivider.TabIndex = 29;
+            this.label_XPDivider.Text = "/";
+            // 
+            // label_AC
+            // 
+            this.label_AC.AutoSize = true;
+            this.label_AC.Location = new System.Drawing.Point(474, 263);
+            this.label_AC.Name = "label_AC";
+            this.label_AC.Size = new System.Drawing.Size(21, 13);
+            this.label_AC.TabIndex = 30;
+            this.label_AC.Text = "AC";
+            // 
+            // label_DC
+            // 
+            this.label_DC.AutoSize = true;
+            this.label_DC.Location = new System.Drawing.Point(70, 590);
+            this.label_DC.Name = "label_DC";
+            this.label_DC.Size = new System.Drawing.Size(22, 13);
+            this.label_DC.TabIndex = 31;
+            this.label_DC.Text = "DC";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(571, 306);
+            this.progressBar1.Maximum = 4;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(58, 12);
+            this.progressBar1.Step = 4;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 2;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(642, 306);
+            this.progressBar2.Maximum = 4;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(58, 12);
+            this.progressBar2.Step = 4;
+            this.progressBar2.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar2.TabIndex = 32;
+            this.progressBar2.Value = 2;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(713, 306);
+            this.progressBar3.Maximum = 4;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(58, 12);
+            this.progressBar3.Step = 4;
+            this.progressBar3.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar3.TabIndex = 33;
+            this.progressBar3.Value = 2;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(784, 306);
+            this.progressBar4.Maximum = 4;
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(58, 12);
+            this.progressBar4.Step = 4;
+            this.progressBar4.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar4.TabIndex = 34;
+            this.progressBar4.Value = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1232, 611);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1204, 1061);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label_DC);
+            this.Controls.Add(this.label_AC);
+            this.Controls.Add(this.label_XPDivider);
+            this.Controls.Add(this.textBox_XPNeeded);
+            this.Controls.Add(this.label_ChaMod);
+            this.Controls.Add(this.label_WisMod);
+            this.Controls.Add(this.label_IntMod);
+            this.Controls.Add(this.label_ConMod);
+            this.Controls.Add(this.label_DexMod);
+            this.Controls.Add(this.label_StrMod);
+            this.Controls.Add(this.textBox_ChaScore);
+            this.Controls.Add(this.textBox_WisScore);
+            this.Controls.Add(this.textBox_IntScore);
+            this.Controls.Add(this.textBox_ConScore);
+            this.Controls.Add(this.textBox_DexScore);
+            this.Controls.Add(this.textBox_StrScore);
+            this.Controls.Add(this.comboBox_Background);
+            this.Controls.Add(this.textBox_HeroPoints);
+            this.Controls.Add(this.textBox_Level);
+            this.Controls.Add(this.textBox_CharacterName);
+            this.Controls.Add(this.textBox_PlayerName);
+            this.Controls.Add(this.textBox_XPCurrent);
+            this.Controls.Add(this.textBox_Size);
+            this.Controls.Add(this.comboBox_Alignment);
+            this.Controls.Add(this.comboBox_Deity);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox_Deity;
+        private System.Windows.Forms.ComboBox comboBox_Alignment;
+        private System.Windows.Forms.TextBox textBox_Size;
+        private System.Windows.Forms.TextBox textBox_XPCurrent;
+        private System.Windows.Forms.TextBox textBox_PlayerName;
+        private System.Windows.Forms.TextBox textBox_CharacterName;
+        private System.Windows.Forms.TextBox textBox_Level;
+        private System.Windows.Forms.TextBox textBox_HeroPoints;
+        private System.Windows.Forms.ComboBox comboBox_Background;
+        private System.Windows.Forms.TextBox textBox_ChaScore;
+        private System.Windows.Forms.TextBox textBox_WisScore;
+        private System.Windows.Forms.TextBox textBox_IntScore;
+        private System.Windows.Forms.TextBox textBox_ConScore;
+        private System.Windows.Forms.TextBox textBox_DexScore;
+        private System.Windows.Forms.TextBox textBox_StrScore;
+        private System.Windows.Forms.Label label_StrMod;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_DexMod;
+        private System.Windows.Forms.Label label_ConMod;
+        private System.Windows.Forms.Label label_IntMod;
+        private System.Windows.Forms.Label label_WisMod;
+        private System.Windows.Forms.Label label_ChaMod;
+        private System.Windows.Forms.TextBox textBox_XPNeeded;
+        private System.Windows.Forms.Label label_XPDivider;
+        private System.Windows.Forms.Label label_AC;
+        private System.Windows.Forms.Label label_DC;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,87 @@ namespace PathFinder_2e_CharacterSheet
 {
     public partial class Form1 : Form
     {
+        // Default values for character
+        private int STR;
+        private int DEX;
+        private int CON;
+        private int INT;
+        private int WIS;
+        private int CHA;
+        private int STRScore;
+        private int DEXScore;
+        private int CONScore;
+        private int INTScore;
+        private int WISScore;
+        private int CHAScore;
+        private int healthPoints;
+        private int armorClass;
+        private int classDC;
+        // Actual values for character
+        private int STR_temp;
+        private int DEX_temp;
+        private int CON_temp;
+        private int INT_temp;
+        private int WIS_temp;
+        private int CHA_temp;
+        private int STRScore_temp;
+        private int DEXScore_temp;
+        private int CONScore_temp;
+        private int INTScore_temp;
+        private int WISScore_temp;
+        private int CHAScore_temp;
+        private int healthPoints_temp;
+        private int armorClass_temp;
+        private int classDC_temp;
+
+        enum proficiency
+        {
+            untrained = 0,
+            trained = 2,
+            expert = 4,
+            master = 6,
+            legendary = 8
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_IntMod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_ChaMod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_ConMod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_DexMod_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_WisMod_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
